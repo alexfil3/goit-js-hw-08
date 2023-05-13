33f8a31a4cdaa3f190401017764d825de6ad6ab8
Alex Filimonov <filimonovalexander9997@gmail.com>
2023-05-13 19:47:26 +0300

    first attempt of homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const imagesMarkup = createMarkup(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createMarkup(galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join('');
+}
+
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    captionsData: "alt",
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from "lodash.throttle";
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('.feedback-form input');
+const messageTextarea = document.querySelector('.feedback-form textarea');
+
+function populateData() {
+    const previousData = JSON.parse(localStorage.getItem("feedback-form-state"))
+
+    if (previousData) {
+        emailInput.value = previousData.email;
+        messageTextarea.value = previousData.message;
+    }
+}
+
+populateData()
+
+let formData = {
+    email: '',
+    message: '',
+}
+
+feedbackForm.addEventListener('submit', onSubmit);
+emailInput.addEventListener('input', throttle(onEmailInput, 500));
+messageTextarea.addEventListener('input', throttle(onMessageTextarea, 500));
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if (formData.email === '' || formData.message === '') {
+        alert("Please fill in all fields.")
+        return
+    } else {
+        localStorage.removeItem("feedback-form-state");
+    }
+        console.log(formData)
+        feedbackForm.reset()
+    };
+
+function onEmailInput() {
+    formData.email = emailInput.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+}
+
+function onMessageTextarea() {
+    formData.message = messageTextarea.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+}
